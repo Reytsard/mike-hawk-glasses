52422f9edb614b7c90693fbb2d9178501f45d84b
RONALD DE MESA <ronaldreyjr061201@gmail.com>
2025-05-05 11:32:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+const [buh, setBuh] = useState({});
+
+
+
+function App() {
+  useEffect(() => {
+    const response = axios.get("localhost:5050/get")
+    const data = response.json()
+    setBuh(data.buh)
+  },[]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
